dcf17eeebff7b0d1f0b13df08c0a51d0de19995f
Gabriel Cordeiro <gabriel.cord18@gmail.com>
2024-01-16 16:33:07 -0300

    incluindo campo de busca

--- src/App.js
+++ src/App.js
@@ -21,12 +21,45 @@ const Container = styled.div`
 const Logo = styled.img`
   width: 350px;
 `;
+const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-top: 10px;
+`;
+
+const SearchInput = styled.input`
+  width: 200px;
+  padding: 8px;
+  border: 1px solid #bbb;
+  border-radius: 5px;
+`;
+
+const SearchButton = styled.button`
+  padding: 8px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  background-color: #00875f;
+  color: white;
+`;
+
+const ClearButton = styled.button`
+  padding: 8px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  background-color: #ff4500;
+  color: white;
+  display: ${(props) => (props.show ? "block" : "none")};
+`;
 
 //const Title = styled.h2``;
 
 function App() {
   const [users, setUsers] = useState([]);
   const [onEdit, setOnEdit] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const getUsers = async () => {
     try {
@@ -71,11 +104,40 @@ function App() {
     getUsers();
   }, [setUsers]);
 
+  const handleSearch = () => {
+    // Aqui você pode adicionar lógica de pesquisa
+    // Por exemplo, filtrar os usuários com base no searchTerm
+    const filteredUsers = users.filter((user) => {
+      const userString = `${user.ano} ${user.capacidade} ${user.mes} ${user.type}`.toLowerCase();
+      return userString.includes(searchTerm.toLowerCase());
+    });
+
+    setUsers(filteredUsers);
+  };
+  const handleClearSearch = () => {
+    setSearchTerm("");
+    getUsers(); // Restaura a lista original de usuários
+  };
+
   return (
     <>
       <Container>
         <Logo src={logoImg}/>
         <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers} />
+
+        <SearchContainer>
+          <SearchInput
+            type="text"
+            placeholder="Digite para buscar..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <SearchButton onClick={handleSearch}>Buscar</SearchButton>
+          <ClearButton show={searchTerm !== ""} onClick={handleClearSearch}>
+            Limpar
+          </ClearButton>
+        </SearchContainer>
+
         <Grid setOnEdit={setOnEdit} users={users} setUsers={setUsers} />
       </Container>
       <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
